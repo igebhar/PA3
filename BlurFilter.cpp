f861428056d08e0163074ef84026279647e757f0
igebhar <37857361+igebhar@users.noreply.github.com>
2018-04-23 16:17:29 -0400

    Added Comments

--- BlurFilter.cpp
+++ BlurFilter.cpp
@@ -14,11 +14,12 @@ BlurFilter::BlurFilter(string name) : KernelFilter(name){}
 BlurFilter::BlurFilter(const BlurFilter& f) :
   KernelFilter(f.name)  {}
 
-
+//this matrix will help blur the image
 const Matrix BlurFilter::Blur =
 {{0.0625,0.125,0.0625},
  {0.125,0.25,0.125},
  {0.0625,0.125,0.0625}};
+
 
 void BlurFilter::apply(Image& img) const{
   KernelFilter::apply(img, Blur);
