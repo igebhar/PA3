01dd2d6c594aea5837a1839f1f54d2dafec2985a
darecluval <37857579+darecluval@users.noreply.github.com>
2018-04-24 09:16:04 -0400

    Update BinaryFilter.h

--- BinaryFilter.h
+++ BinaryFilter.h
@@ -16,8 +16,8 @@ using namespace std;
 
 class BinaryFilter : public PixelFilter {
         protected:
-          static const Pixel WHITE(255, 255, 255);
-          static const Pixel BLACK(0, 0, 0);
+          static const Pixel WHITE;
+          static const Pixel BLACK;
 
         public:
           BinaryFilter();
