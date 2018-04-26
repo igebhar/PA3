0fb0f18e8de466dccdb9254103b236a52a998afa
Clare DuVal <37857579+darecluval@users.noreply.github.com>
2018-04-25 21:41:55 -0400

    added comments

--- VFlipFilter.cpp
+++ VFlipFilter.cpp
@@ -15,12 +15,13 @@ VFlipFilter::VFlipFilter(string name) : SimpleFilter(name){}
 VFlipFilter::VFlipFilter(const VFlipFilter& f) :
   SimpleFilter(f.name)  {}
 
-
+//Determines the brightness of each pixel  
 void VFlipFilter::apply(Image& img) const{
   int height = img.header().height();
   int width = img.header().width();
   int tempheight = height;
  int tempwidth = width;
+  //Switches the height and width into a temporary image
   Image copy(img);
   for (int i = 0; i < width; i++) {
   for (int j = 0; j < height; j++) {
