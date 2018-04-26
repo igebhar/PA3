76d5c7f0bd66f29a3e0e2127520f1a1d0cd48ef4
igebhar <37857361+igebhar@users.noreply.github.com>
2018-04-25 21:00:53 -0400

    Added comments

--- BinaryFilter.cpp
+++ BinaryFilter.cpp
@@ -15,10 +15,15 @@ BinaryFilter::BinaryFilter() :
 BinaryFilter::BinaryFilter(const BinaryFilter& bf) :
     PixelFilter(bf.name), pixel1(255, 255, 255), pixel2(0,0,0)  {}
 
+
+//Destructor
 BinaryFilter::~BinaryFilter() {}
 
+//Determines the brightness of each pixel  
 Pixel BinaryFilter::apply_transform(const Pixel& pix) const{
   uint8_t lum = pix.luminance();
+    //if the luminance value is greater than half
+    //of the maximum value then it will set the pixel to black 
     if(lum >(pixel1.r() / 2)){
       return pixel1;
     }
