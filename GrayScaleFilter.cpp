6a947d67cb6a8893976afdd0997625697dbab1ff
igebhar <37857361+igebhar@users.noreply.github.com>
2018-04-25 21:26:59 -0400

    added comments

--- GrayScaleFilter.cpp
+++ GrayScaleFilter.cpp
@@ -7,16 +7,22 @@ igehbar, ckduval
 #include "GrayscaleFilter.h"
 #include "Pixel.h"
 
+//default constructors
 GrayscaleFilter::GrayscaleFilter() :
   PixelFilter("") {}
 
 GrayscaleFilter::GrayscaleFilter(string name) : PixelFilter(name){}
 
 GrayscaleFilter::GrayscaleFilter(const GrayscaleFilter& f) :
-  PixelFilter(f.name)  {}
+  PixelFilter(f.name) {}
+
+// Destructor
 GrayscaleFilter::~GrayscaleFilter() {}
 
-
+//This uses the the 1st suggested algorithm 
+// calls luminance uing the Pixel passed 
+// the value returned from the luminance will be the new Red, Green
+// and Blue channels
  Pixel GrayscaleFilter::apply_transform(const Pixel& pix) const{
   uint8_t lum = pix.luminance();
   Pixel pixelnew(lum,lum,lum);
