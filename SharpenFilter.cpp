aad0c8d5b3f034e907a10b606bb11209751c2995
Clare DuVal <37857579+darecluval@users.noreply.github.com>
2018-04-25 21:48:30 -0400

    comments added

--- SharpenFilter.cpp
+++ SharpenFilter.cpp
@@ -6,6 +6,7 @@ igehbar, ckduval
 
 #include "SharpenFilter.h"
 
+//Constructors
 SharpenFilter::SharpenFilter() :
   KernelFilter("") {}
 
@@ -14,12 +15,14 @@ SharpenFilter::SharpenFilter(string name) : KernelFilter(name){}
 SharpenFilter::SharpenFilter(const SharpenFilter& f) :
   KernelFilter(f.name)  {}
 
-
+//Matrix with vector of vectors
 const Matrix SharpenFilter::K3 =
 {{0,-1,0},
  {-1,5,-1},
  {0,-1,0}};
 
+//Apply function that applies the matrix to
+//each pixel in the image
 void SharpenFilter::apply(Image& img) const{
   KernelFilter::apply(img, K3);
 }
